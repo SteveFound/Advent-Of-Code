ca9933b621e1fb783b00580c1e9e8d140edb1518
Steve Found <steve@example.com>
2022-12-05 23:06:20 +0000

    Documentation

--- day5.php
+++ day5.php
@@ -2,11 +2,15 @@
 declare(strict_types = 1);
 
 /* Define a stack */
-
 class Stack {
     private SplStack $stack;
 
-    // Fill a stack with characters
+    /**
+     * __construct : A stack is created by a string of characters that define a crate in order bottom to top.
+     * @param string $characters
+     * 
+     * @return void
+     */
     public function __construct( string $characters ) {
         $this->stack = new SplStack();
         foreach (str_split($characters) as $value) {
@@ -14,17 +18,30 @@ class Stack {
         }
     }
 
-    // Grab top most crate
+    /**
+     * grabCrate : Grab the top crate of a stack.
+     * 
+     * @return string
+     */
     public function grabCrate(): string {
         return (string)$this->stack->pop();
     }
 
-    // Drop a crate onto this stack
+    /**
+     * dropCrate : Drop a crate onto a stack
+     * @param string $crate
+     * 
+     * @return void
+     */
     public function dropCrate(string $crate): void {
         $this->stack->push($crate);
     }
- 
-    // Look at the top crate
+
+    /**
+     * topCrate : Return the crate on the top of the stack without removing it.
+     * 
+     * @return string
+     */
     public function topCrate(): string {
         if( $this->stack->count() > 0 ) {
             return (string)$this->stack->top();
@@ -33,16 +50,30 @@ class Stack {
         }
     }
 
-    // Move a character from this stack to another stack
+    /**
+     * moveTo : Grab a crate from this stack then drop it onto another.
+     * @param Stack $that
+     * 
+     * @return void
+     */
     public function moveTo( Stack $that ) {
         $that->dropCrate($this->grabCrate());
     }
 }
 
+/**
+ * Class to manage several stacks and move crates between them as instructed.
+ * class: StackManager
+ */
 class StackManager {
     private array $stacks;
 
-    // Array of stack strings
+    /**
+     * __construct : Take an array of strings that define each stack and create an array of stacks from it.
+     * @param array $stackStrings
+     * 
+     * @return void
+     */
     public function __construct(array $stackStrings) {
         //One stack per string
         $stacks = array(count($stackStrings));
@@ -53,18 +84,41 @@ class StackManager {
         }
     }
 
+    /**
+     * doMove : Move a number of crates from one stack to another, one at a time.
+     * @param Stack $from
+     * @param Stack $to
+     * @param int $count
+     * 
+     * @return void
+     */
     private function doMove( Stack $from, Stack $to, int $count ): void {
         for( $idx = 0; $idx < $count; $idx++ ){
             $from->moveTo($to);
         }
     }
 
+    /**
+     * move9000 : Parse a command that defines the number of crates to move from one to the other and move them.
+     * @param string $command
+     * 
+     * @return void
+     */
     public function move9000(string $command): void {
         // Format of command is 'move 2 from 2 to 7'
         list($count,$from,$to) = sscanf($command, "move %d from %d to %d");
         $this->doMove($this->stacks[$from-1], $this->stacks[$to-1], $count);
     }
 
+    /**
+     * move9001 : Parse a command that defines the number of crates to move from one to the other and move them.
+     * The difference from move9000 is that the crates must be added in the order they were on the original stack.
+     * To achieve this, they are moved from the source stack to an empty temporary stack in reverse order then moved from
+     * the temporary stack to the destination stack in reverse order.  
+     * @param string $command
+     * 
+     * @return void
+     */
     public function move9001(string $command): void {
         // Format of command is 'move 2 from 2 to 7'
         $tmpStack = new Stack('');
@@ -73,6 +127,11 @@ class StackManager {
         $this->doMove($tmpStack, $this->stacks[$to-1], $count);
     }
 
+    /**
+     * topCrates : Concatenate the top crates into a string.
+     * 
+     * @return string
+     */
     public function topCrates(): string {
         $output = '';
         foreach($this->stacks as $stack) {
